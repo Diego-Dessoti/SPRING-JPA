1ef0c52aebe3b7944b599839dbe23cb068473a45
diego-dessoti <didessoti12345@gmail.com>
2024-09-11 20:25:16 -0300

    Exception handling

---
+++ curso/src/main/java/com/diegodessoti/curso/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.diegodessoti.curso.resources.exceptions;
+
+import com.diegodessoti.curso.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status.value()).body(standardError);
+    }
+}
--- curso/src/main/java/com/diegodessoti/curso/services/UserService.java
+++ curso/src/main/java/com/diegodessoti/curso/services/UserService.java
@@ -2,10 +2,12 @@ package com.diegodessoti.curso.services;
 
 import com.diegodessoti.curso.entities.User;
 import com.diegodessoti.curso.repositories.UserRepository;
+import com.diegodessoti.curso.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,7 +20,8 @@ public class UserService {
     }
 
     public User findById(Long id) {
-        return userRepository.findById(id).get();
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User create(User user) {
